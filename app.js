dad7d3a8d76dc8b0633f7c349f5b1df0c5ca6d3e
tobvril <chewavril8@gmail.com>
2024-11-06 15:15:14 +0800

    ss
    ss

---
+++ app.js
@@ -0,0 +1,6 @@
+const instructor = require("./instructor.js");
+
+instructor.addinstruc("test", "1");
+
+const instructor0 = instructor.getinstruc(0);
+console.log("instructor 0 is:", instructor0);
---
+++ instructor.js
@@ -0,0 +1,14 @@
+// manage instructor
+
+module.exports = {
+    instructors: [],
+
+    addinstruc: function (name, mod) {
+        const newinstruc = { name, mod };
+        this.instructors.push(newinstruc);
+    },
+
+    getinstruc: function (index) {
+        return this.instructors[index] || null;
+    }
+};
