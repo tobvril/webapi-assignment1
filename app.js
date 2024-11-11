e7e619ec14c7c9fe1a6836c95abf39a51e612c0c
tobvril <chewavril8@gmail.com>
2024-11-11 11:59:14 +0800

    idk

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const instructor = require("./instructor.js");
 
-instructor.addinstruc("test", "1");
+instructor.addinstruc("donald", "duck", "EGL123", ["L1", "L9", "L5"], "SEG");
 
-const instructor0 = instructor.getinstruc(0);
-console.log("instructor 0 is:", instructor0);
+
+const firstname = instructor.getinstrucfirstname(0);  
+const lastname = instructor.getinstruclastname(0);    
+const imodule = instructor.getinstrucmod(0);           
+const classes = instructor.getinstrucclass(0);        
+const school = instructor.getinstrucschool(0);       
+
+console.log(`${firstname} ${lastname} teaches ${imodule} at ${school}.`);
+console.log(`${firstname} is teaching the following classes: ${classes}`);
--- instructor.js
+++ instructor.js
@@ -1,12 +1,52 @@
 module.exports = {
     instructors: [],
 
-    addinstruc: function (name, mod) {
-        const newinstruc = { name, mod };
+    addinstruc: function (firstname, lastname, module, classes, school) {
+        const newinstruc = { firstname, lastname, module, classes, school };
         this.instructors.push(newinstruc);
     },
 
-    getinstruc: function (index) {
-        return this.instructors[index] || null;
+
+    getinstrucfirstname: function (index) {
+        const instructor = this.instructors[index];
+        if (instructor) {
+            return instructor.firstname;
+        }
+        return null;  
+    },
+
+
+    getinstruclastname: function (index) {
+        const instructor = this.instructors[index];
+        if (instructor) {
+            return instructor.lastname;
+        }
+        return null;
+    },
+
+  
+    getinstrucmod: function (index) {
+        const instructor = this.instructors[index];
+        if (instructor) {
+            return instructor.module;
+        }
+        return null;
+    },
+
+
+    getinstrucclass: function (index) {
+        const instructor = this.instructors[index];
+        if (instructor && Array.isArray(instructor.classes)) {
+            return instructor.classes.join(", ");  
+        }
+        return null; 
+    },
+
+    getinstrucschool: function (index) {
+        const instructor = this.instructors[index];
+        if (instructor) {
+            return instructor.school;
+        }
+        return null;
     }
 };
